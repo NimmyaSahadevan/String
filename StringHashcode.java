ec30da034327e63deb96ad00bf8699580c064178
NimmyaSahadevan <58411427+NimmyaSahadevan@users.noreply.github.com>
2019-12-31 15:39:08 +0530

    Program using Hashcode function

---
+++ StringHashcode.java
@@ -0,0 +1,22 @@
+
+public class StringHashcode 
+{
+
+	public static void main(String[] args) 
+	{
+		String s1,s2;
+		s1=new String("Nimmya");
+		s2=new String("Nimmya");
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s1==s2);
+		System.out.println(s1.hashCode());
+		System.out.println(s2.hashCode());
+		s2=s1+s2;
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s1.hashCode());
+		System.out.println(s2.hashCode());
+	}
+
+}
